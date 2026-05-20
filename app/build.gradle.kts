0ac4274f17b6d35c88823f8fce8565f0738d3910
gionano <intercelroxi@gmail.com>
2026-05-20 14:28:26 +0700

    Update app/build.gradle.kts for dynamic theme system

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,7 @@ dependencies {
     implementation(libs.androidx.media3.exoplayer)
     implementation(libs.androidx.media3.ui)
     implementation(libs.android.image.cropper)
+    implementation(libs.androidx.datastore.preferences)
 }
 
 kotlin {
